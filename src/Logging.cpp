d85c9e529bca3d54e544b116c427a80e24514f9b
PurpleAzurite <misagh.asgari@gmail.com>
2022-02-22 08:16:27 +0300

    Logging is now restricted to Debug builds

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -7,11 +7,13 @@ std::shared_ptr<spdlog::logger> Logger::s_engineLogger;
 
 void Logger::init()
 {
+    #ifdef DEBUG_BUILD
     if (!s_engineLogger)
     {
         s_engineLogger = spdlog::stdout_color_mt("Engine");
         s_engineLogger->info("Logger initialized");
     }
+    #endif
 }
 
 }
--- src/Logging.h
+++ src/Logging.h
@@ -3,7 +3,7 @@
 // clang-format off
 #include <spdlog/logger.h>
 #include <memory>
-// clang-format off
+// clang-format on
 
 namespace Engine {
 
@@ -22,8 +22,14 @@ private:
     static std::shared_ptr<spdlog::logger> s_engineLogger;
 };
 
-}
+} // namespace Engine
 
+#ifdef DEBUG_BUILD
 #define ENGINE_INFO(...) Engine::Logger::engine()->info(__VA_ARGS__)
 #define ENGINE_WARN(...) Engine::Logger::engine()->warn(__VA_ARGS__)
 #define ENGINE_ERROR(...) Engine::Logger::engine()->error(__VA_ARGS__)
+#else
+#define ENGINE_INFO(...)
+#define ENGINE_WARN(...)
+#define ENGINE_ERROR(...)
+#endif
