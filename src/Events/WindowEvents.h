26e7937a62b2eff8df0f412950dc03791a79dea5
PurpleAzurite <misagh.asgari@gmail.com>
2022-02-22 08:16:40 +0300

    Add logging message

--- src/Events/WindowEvents.h
+++ src/Events/WindowEvents.h
@@ -1,6 +1,9 @@
 #pragma once
 
+// clang-format off
 #include "Event.h"
+#include "../Logging.h"
+// clang-format on
 
 namespace Engine {
 
@@ -8,7 +11,7 @@ class WindowClosedEvent : public Event
 {
 public:
     EVENT_CLASS_TYPE(windowClosed);
-    void log() {}
+    void log() { ENGINE_INFO("Window closed"); }
 };
 
 } // namespace Engine
